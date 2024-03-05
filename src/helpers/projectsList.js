5a3037bdc73bffe88bc9819fd048ab6274caffcf
KDA <KDA@d.c>
2024-03-05 17:36:39 +0300

    Add new project

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -22,7 +22,41 @@ import project07Big from "./../img/projects/07-big.png";
 import project08 from "./../img/projects/08.png";
 import project08Big from "./../img/projects/08-big.png";
 
+import project09 from "./../img/projects/09.png";
+import project09Big from "./../img/projects/09-big.png";
+
+import project10 from "./../img/projects/10.png";
+import project10Big from "./../img/projects/10-big.png";
+
 const projects = [
+	{
+		title: 'Bionik',
+		img: project07,
+		imgBig: project07Big,
+		skills: 'HTML, CSS, JavaScript',
+		gitHubLink: 'https://github.com/DanilKur/Bionik'
+	},
+	{
+		title: 'Funiro',
+		img: project08,
+		imgBig: project08Big,
+		skills: 'HTML, CSS, JavaScript',
+		gitHubLink: 'https://github.com/DanilKur'
+	},
+	{
+		title: 'Voodoo',
+		img: project09,
+		imgBig: project09Big,
+		skills: 'HTML, CSS, JavaScript, Rest API',
+		gitHubLink: 'https://github.com/DanilKur/Test.voodoo.co'
+	},
+	{
+		title: 'Dashboard',
+		img: project10,
+		imgBig: project10Big,
+		skills: 'HTML, CSS',
+		gitHubLink: 'https://github.com/DanilKur/Dashboard'
+	},
 	{
 		title: 'Expense',
 		img: project01,
@@ -64,20 +98,6 @@ const projects = [
 		imgBig: project06Big,
 		skills: 'HTML, CSS, React',
 		gitHubLink: 'https://github.com/DanilKur/react-age'
-	},
-	{
-		title: 'Bionik',
-		img: project07,
-		imgBig: project07Big,
-		skills: 'HTML, CSS, JavaScript',
-		gitHubLink: 'https://github.com/DanilKur/Bionik'
-	},
-	{
-		title: 'Funiro',
-		img: project08,
-		imgBig: project08Big,
-		skills: 'HTML, CSS, JavaScript',
-		gitHubLink: 'https://github.com/DanilKur'
 	},
 ];
 
