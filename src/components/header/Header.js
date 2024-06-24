7f9c4c51e6b895867e4df7c995572e4a33c7f51a
DanilKur <120027954+DanilKur@users.noreply.github.com>
2024-06-24 14:18:49 +0300

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
 				<div className="header__text">
 					<p>with passion for learning and creating.</p>
 				</div>
-				<a href="https://cv.djinni.co/fc/56a5151512aefa79260269fdb1900c/Danil_Kurinnyi__Front-End_Engineer__.pdf" className="btn">
+				<a href="https://cv.djinni.co/28/51fb2a01aafe4f380231432807ab1e/Danil_Kurinnyi__Front-End_Engineer__.pdf" className="btn">
 					Download CV
 				</a>
 			</div>
