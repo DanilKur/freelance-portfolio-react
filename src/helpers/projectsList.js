85161fff239b64ae00dfae9b2fafecc7aa05616f
Danil Kur <danilkau311@gmail.com>
2024-09-06 17:21:40 +0300

    update

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -7,15 +7,6 @@ import project02Big from "./../img/projects/02-big.png";
 import project03 from "./../img/projects/03.png";
 import project03Big from "./../img/projects/03-big.png";
 
-import project04 from "./../img/projects/04-.png";
-import project04Big from "./../img/projects/04-big.png";
-
-import project05 from "./../img/projects/05.png";
-import project05Big from "./../img/projects/05-big.png";
-
-import project06 from "./../img/projects/06.png";
-import project06Big from "./../img/projects/06-big.png";
-
 import project07 from "./../img/projects/07.png";
 import project07Big from "./../img/projects/07-big.png";
 
@@ -25,8 +16,6 @@ import project08Big from "./../img/projects/08-big.png";
 import project09 from "./../img/projects/09.png";
 import project09Big from "./../img/projects/09-big.png";
 
-import project10 from "./../img/projects/10.png";
-import project10Big from "./../img/projects/10-big.png";
 
 const projects = [
 	{
@@ -51,13 +40,6 @@ const projects = [
 		gitHubLink: 'https://github.com/DanilKur/Test.voodoo.co'
 	},
 	{
-		title: 'Dashboard',
-		img: project10,
-		imgBig: project10Big,
-		skills: 'HTML, CSS',
-		gitHubLink: 'https://github.com/DanilKur/Dashboard'
-	},
-	{
 		title: 'Expense',
 		img: project01,
 		imgBig: project01Big,
@@ -77,27 +59,6 @@ const projects = [
 		imgBig: project03Big,
 		skills: 'HTML, CSS, React',
 		gitHubLink: 'https://github.com/DanilKur/react-post'
-	},
-	{
-		title: 'Redux cart',
-		img: project04,
-		imgBig: project04Big,
-		skills: 'HTML, CSS, React, Redux, Firebase',
-		gitHubLink: 'https://github.com/DanilKur/react-redux'
-	},
-	{
-		title: 'React form',
-		img: project05,
-		imgBig: project05Big,
-		skills: 'HTML, CSS, React',
-		gitHubLink: 'https://github.com/DanilKur/project-react-form'
-	},
-	{
-		title: 'React age',
-		img: project06,
-		imgBig: project06Big,
-		skills: 'HTML, CSS, React',
-		gitHubLink: 'https://github.com/DanilKur/react-age'
 	},
 ];
 
